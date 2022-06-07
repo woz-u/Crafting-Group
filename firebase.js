d240c63bb411095b031a2abfc0fd5b6c9f731293
Sue Darby <sue.a.darby@gmail.com>
2022-06-06 19:56:16 -0800

    index about firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIyZnXpjje_FIiztLI03zXysgBP8A0G0c",
+  authDomain: "crafting-pm.firebaseapp.com",
+  projectId: "crafting-pm",
+  storageBucket: "crafting-pm.appspot.com",
+  messagingSenderId: "858385904193",
+  appId: "1:858385904193:web:f7d2acd04ddc8d1246ccc0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
