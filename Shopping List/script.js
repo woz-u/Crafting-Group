160f104c3e26a157fda6b70d909dcc2c48e384b9
staciedwyatt <94587981+staciedwyatt@users.noreply.github.com>
2022-06-09 16:01:06 -0500

    Removed delete button. Still working out calculations. Total now has an input field.
    Removed the delete button until I figure it out. Still working out the calculations. Need a break.

--- Shopping List/script.js
+++ Shopping List/script.js
@@ -7,4 +7,32 @@
 //         document.getElementById("result").innerHTML = num1 * num2;
 // }
 
-//attempt #2//
+//attempt #2
+
+//<script>
+//     function myFunction() {
+//         var price = document.getElementById("price").value;
+//         var delivery = document.getElementById("quantity").value;
+//         var total = price * quantity
+//         document.getElementById("totalPrice").innerHTML = total;
+         
+//     }
+// </script>
+
+//attempt #3
+
+//<script>{
+// var quantity = document.getElementById("quantity").value;
+// var price = document.getElementById("price").value;
+
+// function (totalPrice) {
+// document.getElementById("total").value = price * quantity;
+// }
+// </script>
+
+//attempt #4
+// var quantity = document.getElementById("quantity").value;
+// var price = document.getElementById("price").value;
+// function totalPrice(price, quantity){
+//         return price * quantity;
+// }
