3f193b756bffaee8c59fa777de1a97572d4e0be7
staciedwyatt <94587981+staciedwyatt@users.noreply.github.com>
2022-06-09 15:11:19 -0500

    Still struggling with the idea list but these are the newest changes
    Shopping list.

---
+++ Shopping List/script.js
@@ -0,0 +1,10 @@
+// attempt #1 to multiply totals
+
+//function multiplyBy()
+// {
+//         num1 = document.getElementById("quantity").value;
+//         num2 = document.getElementById("price").value;
+//         document.getElementById("result").innerHTML = num1 * num2;
+// }
+
+//attempt #2//
