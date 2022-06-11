48ef86ef97dd856eeb6071c0927a23dfd51feb3f
Sue Darby <sue.a.darby@gmail.com>
2022-06-11 10:17:54 -0800

    more firebase

--- firebase.js
+++ firebase.js
@@ -2,8 +2,12 @@
 import { initializeApp } from "/firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "/firebase/auth";
-import { getFirestore } from "/firebase/firestore";
+import { getFirestore, collection, getDocs } from '/firebase/firestore/lite';
+
+
+
+// import { getAuth, signInWithPopup, GoogleAuthProvider } from "/firebase/auth";
+// import { getFirestore } from "/firebase/firestore";
 
 // Your web app's Firebase configuration for Crafters PM
 const firebaseConfig = {
@@ -14,6 +18,94 @@ const firebaseConfig = {
   messagingSenderId: "1096507400942",
   appId: "1:1096507400942:web:9010c10809725443ad49ff"
 };
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+
+const app = initializeApp(firebaseConfig);
+import { collection, addDoc } from "firebase/firestore"; 
+
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+
+// Add a second document with a generated ID.
+import { addDoc, collection } from "firebase/firestore"; 
+
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Alan",
+    middle: "Mathison",
+    last: "Turing",
+    born: 1912
+  });
+
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+
+import { collection, getDocs } from "firebase/firestore"; 
+
+const querySnapshot = await getDocs(collection(db, "users"));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+});
+
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
+
+// Allow read/write access on all documents to any user signed in to the application
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if request.auth != null;
+    }
+  }
+}
+
+
+// const auth = getAuth();
+// signInWithPopup(auth, provider)
+//   .then((result) => {
+//     // This gives you a Google Access Token. You can use it to access the Google API.
+//     const credential = GoogleAuthProvider.credentialFromResult(result);
+//     const token = credential.accessToken;
+//     // The signed-in user info.
+//     const user = result.user;
+//     // ...
+//   }).catch((error) => {
+//     // Handle Errors here.
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // The email of the user's account used.
+//     const email = error.customData.email;
+//     // The AuthCredential type that was used.
+//     const credential = GoogleAuthProvider.credentialFromError(error);
+//     // ...
+//   });
+
+//   const firebase = require("firebase");
+// // Required for side-effects
+// require("/firebase/firestore");
+
+// const db = getFirestore(app);
+
+import { GoogleAuthProvider } from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const auth = getAuth();
 signInWithPopup(auth, provider)
@@ -34,11 +126,66 @@ signInWithPopup(auth, provider)
     const credential = GoogleAuthProvider.credentialFromError(error);
     // ...
   });
+  
+// Step 1.
+// User tries to sign in to Google.
+auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).catch(function(error) {
+  // An error happened.
+  if (error.code === 'auth/account-exists-with-different-credential') {
+    // Step 2.
+    // User's email already exists.
+    // The pending Google credential.
+    var pendingCred = error.credential;
+    // The provider account's email address.
+    var email = error.email;
+    // Get sign-in methods for this email.
+    auth.fetchSignInMethodsForEmail(email).then(function(methods) {
+      // Step 3.
+      // If the user has several sign-in methods,
+      // the first method in the list will be the "recommended" method to use.
+      if (methods[0] === 'password') {
+        // Asks the user their password.
+        // In real scenario, you should handle this asynchronously.
+        var password = promptUserForPassword(); // TODO: implement promptUserForPassword.
+        auth.signInWithEmailAndPassword(email, password).then(function(result) {
+          // Step 4a.
+          return result.user.linkWithCredential(pendingCred);
+        }).then(function() {
+          // Google account successfully linked to the existing Firebase user.
+          goToApp();
+        });
+        return;
+      }
+      // All the other cases are external providers.
+      // Construct provider object for that provider.
+      // TODO: implement getProviderForProviderId.
+      var provider = getProviderForProviderId(methods[0]);
+      // At this point, you should let the user know that they already have an account
+      // but with a different provider, and let them validate the fact they want to
+      // sign in with this provider.
+      // Sign in to provider. Note: browsers usually block popup triggered asynchronously,
+      // so in real scenario you should ask the user to click on a "continue" button
+      // that will trigger the signInWithPopup.
+      auth.signInWithPopup(provider).then(function(result) {
+        // Remember that the user may have signed in with an account that has a different email
+        // address than the first one. This can happen as Firebase doesn't control the provider's
+        // sign in flow and the user is free to login using whichever account they own.
+        // Step 4b.
+        // Link to Google credential.
+        // As we have access to the pending credential, we can directly call the link method.
+        result.user.linkAndRetrieveDataWithCredential(pendingCred).then(function(usercred) {
+          // Google account successfully linked to the existing Firebase user.
+          goToApp();
+        });
+      });
+    });
+  }
+});
+import { getAuth, signOut } from "firebase/auth";
 
-//   const firebase = require("firebase");
-// // Required for side-effects
-// require("/firebase/firestore");
-
-// const db = getFirestore(app);
-
-// const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+signOut(auth).then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
