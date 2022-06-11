146e0e7b7d33024ce5085e51267f4b71119ec0b1
Sue Darby <sue.a.darby@gmail.com>
2022-06-11 09:38:13 -0800

    firebase

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "/firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "            ";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "/firebase/auth";
+import { getFirestore } from "/firebase/firestore";
 
 // Your web app's Firebase configuration for Crafters PM
 const firebaseConfig = {
@@ -35,10 +35,10 @@ signInWithPopup(auth, provider)
     // ...
   });
 
-  const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
+//   const firebase = require("firebase");
+// // Required for side-effects
+// require("/firebase/firestore");
 
-const db = getFirestore(app);
+// const db = getFirestore(app);
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
