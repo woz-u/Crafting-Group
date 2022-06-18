c43805a775187d642c4495ad4d94e1d8ca73cbfd
Sue Darby <sue.a.darby@gmail.com>
2022-06-18 09:54:14 -0800

    templates in place, correct firebase.js ready task-list template functional

--- src/controllers/firebase.js
+++ src/controllers/firebase.js
@@ -1,3 +1,5 @@
+//Crafting PM Firebase
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
