4924e7565da9aed1f17e2650d3b4230e7a3c4678
Sue Darby <sue.a.darby@gmail.com>
2022-06-19 19:12:26 -0800

    routing

--- src/controllers/firebase.js
+++ src/controllers/firebase.js
@@ -24,10 +24,36 @@ const firebaseConfig = {
 //CRUD for FireStore
 
 //POPUP Auth for Google
+import { GoogleAuthProvider } from "firebase/auth";
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
+const provider = new GoogleAuthProvider();
 
 export {db}
