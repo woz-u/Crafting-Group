b1223fe8b89e5e880f62207a11b26b52d58575c0
Sue Darby <sue.a.darby@gmail.com>
2022-07-03 17:57:41 -0800

    ensuring className vs just class

--- src/components/header.js
+++ src/components/header.js
@@ -35,24 +35,24 @@ function Header() {
 
   return (
     <div>
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
+<nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
   {/* <Link to="./home" className="navbar-brand" >REPLACE WITH LOGO Crafting Project Manager (src/components/header.js/</Link> */}
   <Home />
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
         {/* <Link to="./index" className="nav-link active" aria-current="page" >Project Dashboard</Link> */}
         <Index />
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
         {/* <Link to="./features" className="nav-link">Features</Link> */}
         <Features />
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
         {/* <Link to="./about" className="nav-link">About</Link> */}
         <About />
         </li>
@@ -62,9 +62,9 @@ function Header() {
 
 
 
-    <span class="navbar-text ">
-        <button class="btn btn-primary " onClick={googleSignIn}> Sign in with Google</button>
-        <button class="btn btn-primary " onClick={logOut}>Log out</button>
+    <span className="navbar-text ">
+        <button className="btn btn-primary " onClick={googleSignIn}> Sign in with Google</button>
+        <button className="btn btn-primary " onClick={logOut}>Log out</button>
     </span>
     </nav> 
       </div>
